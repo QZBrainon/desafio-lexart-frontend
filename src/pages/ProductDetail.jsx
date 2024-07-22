2950cfe15dcb4998de17c33325df7bcc74ab87e4
QZBrainon <brainonqueiroz@gmail.com>
2024-07-22 06:26:16 -0300

    try01

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -32,7 +32,7 @@ function ProductDetail() {
 
   return (
     <div>
-      {
+      {product ?? (
         <section className="text-gray-600 body-font overflow-hidden">
           <div className="container px-5 py-24 mx-auto">
             <div className="lg:w-4/5 mx-auto flex flex-wrap">
@@ -209,7 +209,7 @@ function ProductDetail() {
             </div>
           </div>
         </section>
-      }
+      )}
     </div>
   );
 }
