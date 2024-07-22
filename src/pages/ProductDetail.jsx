08e8f92e3ac4bc2fb999dbcdaa66a80a68603f41
QZBrainon <brainonqueiroz@gmail.com>
2024-07-22 06:28:24 -0300

    try02

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -32,7 +32,7 @@ function ProductDetail() {
 
   return (
     <div>
-      {product ?? (
+      {product && (
         <section className="text-gray-600 body-font overflow-hidden">
           <div className="container px-5 py-24 mx-auto">
             <div className="lg:w-4/5 mx-auto flex flex-wrap">
