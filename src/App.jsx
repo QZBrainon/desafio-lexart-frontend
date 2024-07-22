c5ab01d2df719498c39cc32c4de94afad9271b74
QZBrainon <brainonqueiroz@gmail.com>
2024-07-22 04:01:11 -0300

    added delete feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Products from "./pages/Products";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/products" element={<Products />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,25 +1,43 @@
 import React from "react";
 import { LuTrash2 } from "react-icons/lu";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function ProductCard({ id, name, brand, model, price, color }) {
+  const navigate = useNavigate();
+  const deleteItem = async (id) => {
+    try {
+      await axios.delete("https://desafio-lexart-backend.vercel.app/products", {
+        id,
+      });
+      navigate("/products");
+    } catch (error) {
+      navigate("/404");
+    }
+  };
   return (
     <>
-      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a class="block relative h-48 rounded overflow-hidden">
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full" id={id}>
+        <a className="block relative h-48 rounded overflow-hidden">
           <img
             alt="ecommerce"
-            class="object-cover object-center w-full h-full block"
+            className="object-cover object-center w-full h-full block"
             src="https://dummyimage.com/420x260"
           />
         </a>
-        <div class="mt-4">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
             {brand} - {model} - {color}
           </h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">{name}</h2>
-          <div className="flex justify-between items-center">
-            <p class="mt-1">${price}</p>
-            <LuTrash2 className="fill-red-600" />
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+            {name}
+          </h2>
+          <div classNameName="flex justify-between items-center">
+            <p className="mt-1">${price}</p>
+            <LuTrash2
+              onClick={(e) => deleteItem(e.target.id)}
+              classNameName="stroke-red-600"
+            />
           </div>
         </div>
       </div>
